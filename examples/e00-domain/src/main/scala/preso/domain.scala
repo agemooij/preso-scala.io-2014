dd1e33df234d1bf964e3fff9683d4670f2fc616e
Age Mooij <age.mooij@gmail.com>
2014-10-22 12:19:49 +0200

    Added the Dude domain

--- examples/e00-domain/src/main/scala/preso/domain.scala
+++ examples/e00-domain/src/main/scala/preso/domain.scala
@@ -8,3 +8,9 @@ case class Thing(name: String)
 object Thing {
   implicit val json = jsonFormat1(Thing.apply)
 }
+
+case class Dude(id: String, name: String, tags: Set[String] = Set.empty)
+
+object Dude {
+  implicit val json = jsonFormat3(Dude.apply)
+}
